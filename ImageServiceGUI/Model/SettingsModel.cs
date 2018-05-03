eac52c9f0a0fd919ce1fb699a7ea5f05f531e606
tamar emanuel <tamarema@gmail.com>
2018-05-03 22:48:49 +0300

    GUI

--- ImageServiceGUI/Model/SettingsModel.cs
+++ ImageServiceGUI/Model/SettingsModel.cs
@@ -8,5 +8,26 @@ namespace ImageServiceGUI.Model
 {
     class SettingsModel
     {
+        private string output_directory;
+        public string outputDirectory
+        {
+            get { return outputDirectory; }
+            set
+            {
+                outputDirectory = value;
+                OnPropertyChanged("Dirctory");
+            }
+        }
+
+        private string source_name;
+        public string sourceName
+        {
+            get { return source_name; }
+            set
+            {
+                outputDirectory = value;
+                OnPropertyChanged("Name");
+            }
+        }
     }
 }
